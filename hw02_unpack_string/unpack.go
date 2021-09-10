b238d05523cff94207992b7ef85b41daa1df2cef
Alexandr Lakeev <lakeev.alexandr@gmail.com>
2021-09-10 10:02:58 +0300

    unpuck function complete

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var (
+		sb      strings.Builder
+		last    string
+		escaped bool
+	)
+
+	for _, r := range s {
+		switch {
+		case !escaped && r == '\\':
+			escaped = true
+		case !escaped && unicode.IsDigit(r):
+			n, err := strconv.Atoi(string(r))
+			if err != nil {
+				return "", err
+			}
+			if last == "" {
+				return "", ErrInvalidString
+			}
+			sb.WriteString(strings.Repeat(last, n))
+			last = ""
+			escaped = false
+		default:
+			sb.WriteString(last)
+			last = string(r)
+			escaped = false
+		}
+	}
+
+	sb.WriteString(last)
+
+	return sb.String(), nil
 }
