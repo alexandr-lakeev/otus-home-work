4870cbf056eb00e19794de12c6b946f86e97bc82
Alexandr Lakeev <lakeev.alexandr@gmail.com>
2021-09-24 09:58:34 +0300

    list complete

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,92 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front *ListItem
+	back  *ListItem
+	len   int
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	i := &ListItem{Value: v}
+
+	if l.Len() == 0 {
+		l.InitList(i)
+		return i
+	}
+
+	l.PushFrontItem(i)
+	l.len++
+
+	return i
+}
+
+func (l *list) InitList(i *ListItem) {
+	l.back, l.front = i, i
+	l.len = 1
+}
+
+func (l *list) PushFrontItem(i *ListItem) {
+	oldFront := l.front
+	i.Next, oldFront.Prev = oldFront, i
+	l.front = i
+}
+
+func (l *list) PushBackItem(i *ListItem) {
+	oldBack := l.back
+	i.Prev, oldBack.Next = oldBack, i
+	l.back = i
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	i := &ListItem{Value: v}
+
+	if l.Len() == 0 {
+		l.InitList(i)
+		return i
+	}
+
+	l.PushBackItem(i)
+	l.len++
+
+	return i
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == l.front {
+		l.front = i.Next
+	}
+
+	if i == l.back {
+		l.back = i.Prev
+	}
+
+	if i.Next != nil && i.Prev != nil {
+		i.Next.Prev, i.Prev.Next = i.Prev, i.Next
+	} else if i.Next != nil {
+		i.Next.Prev = nil
+	} else if i.Prev != nil {
+		i.Prev.Next = nil
+	}
+
+	i.Next, i.Prev = nil, nil
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.PushFrontItem(i)
 }
 
 func NewList() List {
