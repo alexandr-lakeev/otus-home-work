0b9191c5923b929fd3056bdf1aa83a81489fa4ee
Alexandr Lakeev <lakeev.alexandr@gmail.com>
2021-11-19 12:16:00 +0300

    hw08 complete

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +20,47 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	filesInfo, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	envs := make(Environment)
+
+	for _, fi := range filesInfo {
+		name := fi.Name()
+
+		envs[name], err = readEnvValue(fi, dir)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return envs, nil
+}
+
+func readEnvValue(fileInfo os.FileInfo, dir string) (EnvValue, error) {
+	var value EnvValue
+
+	if fileInfo.Size() == 0 {
+		value.NeedRemove = true
+		return value, nil
+	}
+
+	fullpath := path.Join(dir, fileInfo.Name())
+
+	file, err := os.Open(fullpath)
+	if err != nil {
+		return value, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	value.Value = strings.TrimRight(scanner.Text(), " \n\t")
+	value.Value = string(bytes.ReplaceAll([]byte(value.Value), []byte{0x00}, []byte("\n")))
+
+	return value, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	c := exec.Command(cmd[0], cmd[1:]...)
+
+	c.Env = createCmdEnv(env)
+	c.Stdout = os.Stdout
+	c.Stdin = os.Stdin
+
+	err := c.Run()
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+
+	return 0
+}
+
+func createCmdEnv(env Environment) []string {
+	var cmdEnv = os.Environ()
+
+	for name, value := range env {
+		cmdEnv = append(cmdEnv, name+"="+value.Value)
+	}
+
+	return cmdEnv
 }
