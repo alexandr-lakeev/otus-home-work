045ca0b88dfeed22a55e158117cbdc3898284822
Alexandr Lakeev <lakeev.alexandr@gmail.com>
2022-01-16 15:31:48 +0300

    hw15 get events list integration tests init

--- hw12_13_14_15_calendar/tests/create_event_test.go
+++ hw12_13_14_15_calendar/tests/create_event_test.go
@@ -13,17 +13,17 @@ import (
 	"github.com/google/uuid"
 )
 
-type createEventTest struct {
+type calendarTest struct {
 	userID             uuid.UUID
 	responseStatusCode int
 	responseBody       []byte
 }
 
-func (test *createEventTest) createUserId(*messages.Pickle) {
+func (test *calendarTest) createUserId(*messages.Pickle) {
 	test.userID = uuid.New()
 }
 
-func (test *createEventTest) iSendRequestToWithData(httpMethod, addr, contentType string, data *godog.DocString) error {
+func (test *calendarTest) iSendRequestToWithData(httpMethod, addr, contentType string, data *godog.DocString) error {
 	var (
 		r   *http.Response
 		err error
@@ -40,7 +40,7 @@ func (test *createEventTest) iSendRequestToWithData(httpMethod, addr, contentTyp
 		}
 		body := ioutil.NopCloser(strings.NewReader(cleanJson))
 
-		var client = &http.Client{}
+		client := http.Client{}
 		request := &http.Request{
 			Method: httpMethod,
 			URL:    url,
@@ -64,14 +64,49 @@ func (test *createEventTest) iSendRequestToWithData(httpMethod, addr, contentTyp
 	return err
 }
 
-func (test *createEventTest) theResponseCodeShouldBe(code int) error {
+func (test *calendarTest) iSendRequestTo(httpMethod, addr string) error {
+	var (
+		r   *http.Response
+		err error
+	)
+
+	switch httpMethod {
+	case http.MethodGet:
+		url, err := url.Parse(addr)
+		if err != nil {
+			return err
+		}
+
+		client := http.Client{}
+		request := &http.Request{
+			Method: httpMethod,
+			URL:    url,
+			Header: map[string][]string{
+				"X-User-Id": {test.userID.String()},
+			},
+		}
+		r, err = client.Do(request)
+	default:
+		err := fmt.Errorf("unknown method: %s", httpMethod)
+		if err != nil {
+			return err
+		}
+	}
+
+	test.responseStatusCode = r.StatusCode
+	test.responseBody, err = ioutil.ReadAll(r.Body)
+
+	return err
+}
+
+func (test *calendarTest) theResponseCodeShouldBe(code int) error {
 	if test.responseStatusCode != code {
 		return fmt.Errorf("unexpected status code: %d != %d", test.responseStatusCode, code)
 	}
 	return nil
 }
 
-func (test *createEventTest) theErrorShouldBeInTheField(errText, errField string) error {
+func (test *calendarTest) theErrorShouldBeInTheField(errText, errField string) error {
 	response := struct {
 		Data   map[string]interface{} `json:"data"`
 		Errors map[string]string      `json:"errors"`
@@ -89,11 +124,12 @@ func (test *createEventTest) theErrorShouldBeInTheField(errText, errField string
 }
 
 func FeatureContext(s *godog.ScenarioContext) {
-	test := new(createEventTest)
+	test := new(calendarTest)
 
 	s.BeforeScenario(test.createUserId)
 
 	s.Step(`^I send "([^"]*)" request to "([^"]*)" with "([^"]*)" data:$`, test.iSendRequestToWithData)
+	s.Step(`^I send "([^"]*)" request to "([^"]*)"$`, test.iSendRequestTo)
 	s.Step(`^The response code should be (\d+)$`, test.theResponseCodeShouldBe)
 	s.Step(`^The error should be "([^"]*)" in The field "([^"]*)"$`, test.theErrorShouldBeInTheField)
 }
---
+++ hw12_13_14_15_calendar/tests/features/get_events_list.feature
@@ -0,0 +1,12 @@
+Feature: Get Events list
+	Scenario: Events list for a day
+		When I send "GET" request to "http://backend:8080/api/calendar/v1/events?from=2021-12-31T00:00:00Z&to=2021-12-31T23:59:59Z"
+		Then The response code should be 200
+
+	Scenario: Events list for a week
+		When I send "GET" request to "http://backend:8080/api/calendar/v1/events?from=2021-12-27T00:00:00Z&to=2022-01-02T23:59:59Z"
+		Then The response code should be 200
+
+	Scenario: Events list for a month
+		When I send "GET" request to "http://backend:8080/api/calendar/v1/events?from=2021-12-01T00:00:00Z&to=2021-12-31T23:59:59Z"
+		Then The response code should be 200
