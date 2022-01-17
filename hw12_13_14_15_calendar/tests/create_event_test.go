01dbb8ed6892da8b16962cba97b93ba7b28a5287
Alexandr Lakeev <lakeev.alexandr@gmail.com>
2022-01-17 20:48:41 +0300

    hw15 get events list integration tests complete

--- hw12_13_14_15_calendar/tests/create_event_test.go
+++ hw12_13_14_15_calendar/tests/create_event_test.go
@@ -23,6 +23,64 @@ func (test *calendarTest) createUserId(*messages.Pickle) {
 	test.userID = uuid.New()
 }
 
+func (test *calendarTest) addTestEvents(*messages.Pickle) {
+	testEventsJson := []string{
+		`{
+      "title":"Event #1",
+      "date":"2021-12-02T13:00:00Z",
+      "duration":60
+    }`,
+		`{
+      "title":"Event #2",
+      "date":"2021-12-15T14:00:00Z",
+      "duration":60
+    }`,
+		`{
+      "title":"Event #3",
+      "date":"2021-12-28T15:00:00Z",
+      "duration":60
+    }`,
+		`{
+      "title":"Event #4",
+      "date":"2021-12-30T16:00:00Z",
+      "duration":60
+    }`,
+		`{
+      "title":"Event #5",
+      "date":"2021-12-31T18:00:00Z",
+      "duration":60
+    }`,
+	}
+
+	url, err := url.Parse("http://backend:8080/api/calendar/v1/events")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, eventJson := range testEventsJson {
+		body := ioutil.NopCloser(strings.NewReader(eventJson))
+
+		client := http.Client{}
+		request := &http.Request{
+			Method: http.MethodPost,
+			URL:    url,
+			Body:   body,
+			Header: map[string][]string{
+				"Content-Type": {"application/json"},
+				"X-User-Id":    {test.userID.String()},
+			},
+		}
+		r, err := client.Do(request)
+		if err != nil {
+			panic(err)
+		}
+
+		if r.StatusCode != http.StatusOK {
+			panic(fmt.Errorf("test event not created, status code %d", r.StatusCode))
+		}
+	}
+}
+
 func (test *calendarTest) iSendRequestToWithData(httpMethod, addr, contentType string, data *godog.DocString) error {
 	var (
 		r   *http.Response
@@ -123,13 +181,31 @@ func (test *calendarTest) theErrorShouldBeInTheField(errText, errField string) e
 	return nil
 }
 
+func (test *calendarTest) theJsonShouldContainEvents(eventsCount int) error {
+	response := struct {
+		Data []interface{} `json:"data"`
+	}{}
+
+	err := json.Unmarshal([]byte(test.responseBody), &response)
+	if err != nil {
+		return err
+	}
+
+	if len(response.Data) != eventsCount {
+		return fmt.Errorf("unexpected events count: %d != %d", len(response.Data), eventsCount)
+	}
+	return nil
+}
+
 func FeatureContext(s *godog.ScenarioContext) {
 	test := new(calendarTest)
 
 	s.BeforeScenario(test.createUserId)
+	s.BeforeScenario(test.addTestEvents)
 
 	s.Step(`^I send "([^"]*)" request to "([^"]*)" with "([^"]*)" data:$`, test.iSendRequestToWithData)
 	s.Step(`^I send "([^"]*)" request to "([^"]*)"$`, test.iSendRequestTo)
 	s.Step(`^The response code should be (\d+)$`, test.theResponseCodeShouldBe)
 	s.Step(`^The error should be "([^"]*)" in The field "([^"]*)"$`, test.theErrorShouldBeInTheField)
+	s.Step(`^The json should contain (\d+) event[s]?$`, test.theJsonShouldContainEvents)
 }
